7b9bef1f8b7883176a937a7c70e1c4084cad318d
linzx <zhkulzx@163.com>
2024-06-27 16:03:35 +0800

    修改域名

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.zhkulzx.workers.dev": dockerHub,
+  "quay.zhkulzx.workers.dev": "https://quay.io",
+  "gcr.zhkulzx.workers.dev": "https://gcr.io",
+  "k8s-gcr.zhkulzx.workers.dev": "https://k8s.gcr.io",
+  "k8s.zhkulzx.workers.dev": "https://registry.k8s.io",
+  "ghcr.zhkulzx.workers.dev": "https://ghcr.io",
+  "cloudsmith.zhkulzx.workers.dev": "https://docker.cloudsmith.io",
+  "ecr.zhkulzx.workers.dev": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.zhkulzx.workers.dev": dockerHub,
 };
 
 function routeByHosts(host) {
